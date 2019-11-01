72bed3b471fdb882af511435f5056e98f62c5469
cms <moonsuk4@hubit.co.kr>
2019-11-01 16:28:37 +0900

    Initial commit

---
+++ c1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+class NewWinForm5 : Form
+{
+    public NewWinForm5(string strText)
+    {
+        this.Text = strText;
+        this.Load += new System.EventHandler(this.Form_Load);
+        this.FormClosed += new FormClosed.EventHandler(this.Form_Closed);
+        this.Show();
+    }
+    static void Main(string[] args)
+    {
+        Application.Run(new NewWinForm5("이벤트 등록"));
+    }
+    private void Form_Load(object sender, System.EventArgs e)
+    {
+        Console.WriteLine("윈도우가 load됩니다");
+    }
+    private void Form_Closed(object sender, FormClosedEventArgs e)
+    {
+        Console.WriteLine("윈도우가 closed됩니다.");
+    }
+}
